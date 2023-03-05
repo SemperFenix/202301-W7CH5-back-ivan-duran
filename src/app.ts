67cbd8be55ac2cbeb9590d341f585607956ae8f9
SemperFenix <dureivan@gmail.com>
2023-03-05 01:00:48 +0100

    Add error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import createDebug from 'debug';
 import { _dirname } from './config.js';
 import morgan from 'morgan';
 import cors from 'cors';
+import { membersRouter } from './routers/members.router.js';
+import { CustomError } from './errors/http.error.js';
 
 const debug = createDebug('W7B:app');
 
@@ -18,11 +20,27 @@ const corsOrigins = {
 app.use(cors(corsOrigins));
 app.use(express.json());
 
+app.use('/members', membersRouter);
+
 app.use('/', (_req, resp) => {
   resp.json({
     info: 'Social Network Project',
     endpoints: {
-      users: '/users',
+      members: '/members',
     },
   });
 });
+
+app.use(
+  (error: CustomError, _req: Request, resp: Response, _next: NextFunction) => {
+    const status = error.code || 500;
+    const statusMessage = error.outMsg || 'Internal Server Error';
+    resp.status(status);
+    debug('Error: ', status, statusMessage);
+    debug(error.name, ': ', error.message);
+
+    resp.json({
+      error: [{ status, statusMessage }],
+    });
+  }
+);
